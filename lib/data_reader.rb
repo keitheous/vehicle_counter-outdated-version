90034bd780db9fe47918348795a8a2ac8fa84f9d
keitheous <keithchongwy@gmail.com>
2016-08-21 01:53:11 +1000

    created a few test units - initial stage

--- lib/data_reader.rb
+++ lib/data_reader.rb
@@ -1,7 +1,18 @@
 require 'pry'
 
 class DataReader
-  def dataset(data = "../fake_data.txt")
-    File.open(data, "r")
+  attr_accessor :north_bound, :south_bound
+
+  def initialize
+    @north_bound = {}
+    @south_bound = {}
+  end
+
+  def self.source(dataset = "../fake_data.txt")
+    File.size(dataset)
+
+    # binding.pry
   end
 end
+
+# DataReader.source
--- spec/data_reader_test.rb
+++ spec/data_reader_test.rb
@@ -4,12 +4,29 @@ require 'minitest/autorun'
 require_relative '../lib/data_reader'
 require 'minitest/pride'
 
-class DataReaderTest < Minitest::Test
-    def setup
-      @entry = DataReader.new
-    end
+describe "DataReader" do
 
-    def test_for_existance_of_data
-      assert_equal true , @entry.dataset != 0
-    end
+  before do
+    @entry = DataReader.new
+  end
+  #create an initialize function under DataReaderClass
+  it "entry must not equal to nil" do
+    @entry.wont_be_nil
+  end
+
+  #define north_bound as an empty Hash
+  it "north bound defined as empty hash" do
+    @entry.north_bound.must_equal Hash.new
+  end
+
+  #define south_bound as an empty Hash
+  it "south bound defined as empty hash" do
+    @entry.south_bound.must_equal Hash.new
+  end
+
+  #successfully reads .txt file using F.size
+  it "size of dataset should not equal to zero" do
+    DataReader.source.wont_be_nil
+  end
+
 end
