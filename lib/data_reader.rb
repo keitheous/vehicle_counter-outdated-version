7b0d13910776ccbe7b44d602239fa8bea60fd894
keitheous <keithchongwy@gmail.com>
2016-08-20 22:47:27 +1000

    first test unit yay

--- lib/data_reader.rb
+++ lib/data_reader.rb
@@ -1,5 +1,7 @@
 require 'pry'
 
 class DataReader
-
+  def dataset(data = "../fake_data.txt")
+    File.open(data, "r")
+  end
 end
--- spec/data_reader_test.rb
+++ spec/data_reader_test.rb
@@ -5,4 +5,11 @@ require_relative '../lib/data_reader'
 require 'minitest/pride'
 
 class DataReaderTest < Minitest::Test
+    def setup
+      @entry = DataReader.new
+    end
+
+    def test_for_existance_of_data
+      assert_equal true , @entry.dataset != 0
+    end
 end
