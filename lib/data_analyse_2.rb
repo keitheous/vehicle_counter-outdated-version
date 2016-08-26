64b5a90b5496b76d637269f2af50b5ed0f6371f8
keitheous <keithchongwy@gmail.com>
2016-08-26 14:47:24 +1000

    moving onto 30 minsly

--- lib/data_analyse_2.rb
+++ lib/data_analyse_2.rb
@@ -11,37 +11,58 @@ class DataAnalyse
   end
 
   def nb_sort_frequency
-    # nb_hourly = hourly(@nb)
-    # puts nb_hourly
+    nb_hourly = hourly(@nb)
+    puts nb_hourly.count
+
+    # nb_am_vs_pm = compare_am_to_pm(nb_hourly)
   end
 
   def sb_sort_frequency
-    # sb_hourly = hourly(@sb)
+    sb_hourly = hourly(@sb)
     # puts sb_hourly
+    # sb_am_vs_pm = compare_am_to_pm(sb_hourly)
   end
 
   private
 
+  def compare_am_to_pm(subject)
+
+    (1..5).each do |day|
+      morning_count = 0
+      evening_count = 0
+
+      (6..11).each do |morning_hour|
+        morning_count += subject["Day #{day}"][morning_hour].to_i
+      end
+
+      (18..23).each do |evening_hour|
+        evening_count += subject["Day #{day}"][evening_hour].to_i
+      end
+
+      puts "Day #{day}, morning to evening = #{morning_count} to #{evening_count}"
+
+    end
+
+  end
+
   def hourly(subject)
     frequency_day = {}
-
     (1..5).each do |day|
 
       daily_frequency = []
-      puts "Sorting Hourly Day #{day}"
+      # puts "Sorting Hourly Day #{day}"
       day_subject= subject.select{|key, hash| hash["Day"] == day }
 
       ("00"..."24").each do |hour|
         stored_by_hour = day_subject.select{|key, hash| hash["1st UTC"][0] == "#{hour[0]}" && hash["1st UTC"][1] == "#{hour[1]}"}
 
-        time = (hour.to_i + 11) % 12 + 1
-        hour.to_i < 12 ? meridiem = "am" : meridiem = "pm"
-
         daily_frequency.push("#{stored_by_hour.count}")
+        # binding.pry
       end
+      
 
       frequency_day["Day #{day}"] = daily_frequency
-      
+
     end
 
     frequency_day
